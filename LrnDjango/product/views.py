a725eb93d76163696c45e5a2f14f764ef8b9774c
erfan <erfan.yekezare@gmail.com>
2021-03-14 16:15:01 +0330

    Connect [ ProductForm ] to [ product_create_view ]

--- LrnDjango/product/views.py
+++ LrnDjango/product/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context ={
+        "form" : form
+    }
+    return render(request, "product/create.html", context)
 
 def products_view(request):
     obj_list = Product.objects.all()
