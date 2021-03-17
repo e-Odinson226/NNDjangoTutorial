06cfede1fc36b077083ba330636f4fe64a0dd840
erfan <erfan.yekezare@gmail.com>
2021-03-17 18:36:22 +0330

    Creatre a dedicated [urls.py] for products app.

---
+++ LrnDjango/product/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+                            product_single_view,
+                            products_view,
+                            product_create_view,
+                            product_delete_view   )
+app_name = "product"
+urlpatterns = [
+    path('', products_view, name="products"),
+    path('<int:id>/', product_single_view, name="pDetail"),
+    path('create/', product_create_view, name="pCreate"),
+    path('<int:id>/delete', product_delete_view, name="pDelete"),
+
+]
