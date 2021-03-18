f79366a70db663761cc048766e01840bb83e842a
erfan <erfan.yekezare@gmail.com>
2021-03-18 20:27:22 +0330

    Edit and reconfigure [Profile] as producer in shape of model.

--- LrnDjango/producer/models.py
+++ LrnDjango/producer/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
-class Producer(models.Model):
-    name    =   models.CharField(max_length=25)
-    address =   models.CharField(max_length=50)
-    desc    =   models.CharField(max_length=100)
-    reg_id  =   models.IntegerField(unique=True)
-    logo    =   models.ImageField(width_field=600,height_field=600)
+class Profile(models.Model):
+    producer    = models.OneToOneField(User, on_delete=models.CASCADE)
+    logo        = models.ImageField(default="default.jpg", width_field=600, height_field=600, upload_to="logos")
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        logo_pic = Image.open(self.logo.path)
+        if logo_pic.width > 600 or logo_pic.height > 600:
+            max_size = (600, 600)
+            logo_pic.thumbnail(max_size)
+            logo_pic.save(self.logo.path)
