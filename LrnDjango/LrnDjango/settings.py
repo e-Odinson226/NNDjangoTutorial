d2dbb1f8e3c5e82ff30c75bfbdce9d5427d4ce64
erfan <erfan.yekezare@gmail.com>
2021-03-18 20:24:45 +0330

    1.Add crispy forms. 2.Define media root and static files.

--- LrnDjango/LrnDjango/settings.py
+++ LrnDjango/LrnDjango/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'product',
     'producer',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
