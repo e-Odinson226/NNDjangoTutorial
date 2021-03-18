513647ad5d03024ae834b410da8f33201a549e9e
erfan <erfan.yekezare@gmail.com>
2021-03-18 13:22:46 +0330

    Create [producer] model and migrate it.

--- LrnDjango/producer/models.py
+++ LrnDjango/producer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Producer(models.Model):
+    name    =   models.CharField(max_length=25)
+    address =   models.CharField(max_length=50)
+    desc    =   models.CharField(max_length=100)
