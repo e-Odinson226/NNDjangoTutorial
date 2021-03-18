bdbe48328024af77e0167e5cd4221d765e544bd2
erfan <erfan.yekezare@gmail.com>
2021-03-18 13:53:03 +0330

    Reconfigure [producer] model.

--- LrnDjango/producer/models.py
+++ LrnDjango/producer/models.py
@@ -5,3 +5,5 @@ class Producer(models.Model):
     name    =   models.CharField(max_length=25)
     address =   models.CharField(max_length=50)
     desc    =   models.CharField(max_length=100)
+    reg_id  =   models.IntegerField(unique=True)
+    logo    =   models.ImageField(width_field=600,height_field=600)
