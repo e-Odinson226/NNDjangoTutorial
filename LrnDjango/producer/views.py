867f12a33c661d4fa40df14ced3191816d77f00f
erfan <erfan.yekezare@gmail.com>
2021-03-19 15:32:47 +0330

    Create [producer_reg] view.

--- LrnDjango/producer/views.py
+++ LrnDjango/producer/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile
+from .forms import ProducerRegForm
 
-# Create your views here.
+def producer_reg(request):
+    if request.method == "POST":
+        form = ProducerRegForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            return redirect('login')
+    else:
+        form = ProducerRegForm()
+    context ={
+        "form" : form
+    }
+    return render(request, "producer/register.html", context)
+#--------------------------------------------------------------------
