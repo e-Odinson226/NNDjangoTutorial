df9137770026030aecc8d90d3036e02e1f1bb72f
erfan <erfan.yekezare@gmail.com>
2021-03-19 15:39:35 +0330

    Add [date_posted & producer] to product model.

--- LrnDjango/product/models.py
+++ LrnDjango/product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
@@ -7,6 +9,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=100)
     stock       = models.BooleanField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    producer    = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return  reverse("product:pDetail", kwargs={"id":self.id}) #f"/products/{self.id}"
