5d6d933c0735753cb717cc78fe2e38d18cad2776
erfan <erfan.yekezare@gmail.com>
2021-03-24 20:18:43 +0430

    Route log-in, profile, log-out.

--- LrnDjango/producer/urls.py
+++ LrnDjango/producer/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import ( producer_reg )
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import (    register_view,
+                        profile )
 
 app_name = "producer"
 urlpatterns =[
-    path('register/', producer_reg, name="producerDetail")
+    path('register/',   register_view,          name="register"),
+
+    path('profile/',    profile,                name="profile"),
+
+    path('log-in/',     LoginView.as_view(
+        template_name='producer/login.html'),   name="login"),
+
+    path('log-out/',    LogoutView.as_view(
+        template_name='producer/logout.html'),  name="logout"),
+
 ]
