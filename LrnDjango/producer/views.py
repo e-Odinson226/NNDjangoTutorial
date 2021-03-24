fd7f1dc0bcaa6206ba1080e69f604ed2c57fa3b2
erfan <erfan.yekezare@gmail.com>
2021-03-24 20:19:05 +0430

    Add view for profile.

--- LrnDjango/producer/views.py
+++ LrnDjango/producer/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Profile
-from .forms import ProducerRegForm
+from .forms import (    ProducerRegForm,
+                        ProducerUpdForm,
+                        ProfileUpdForm,)
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-def producer_reg(request):
+
+def register_view(request):
     if request.method == "POST":
         form = ProducerRegForm(request.POST)
         if form.is_valid():
@@ -16,3 +21,28 @@ def producer_reg(request):
     }
     return render(request, "producer/register.html", context)
 #--------------------------------------------------------------------
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = ProducerUpdForm(request.POST, instance=request.user)
+        p_form = ProfileUpdForm(    request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            
+            messages.success(request, f'Your acount has been Updated.') 
+            return redirect('profile')
+    else:
+        u_form = ProducerUpdForm(instance=request.user)
+        p_form = ProfileUpdForm(instance=request.user.profile)
+        
+    context = {
+        "u_form" : u_form,
+        "p_form" : p_form
+    }
+    return render(request, 'producer/profile.html', context)
+
+    
